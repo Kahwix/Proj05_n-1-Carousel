922b6a5c3a3a5baa74ac1fb3c323e24eaf9d942d
52999103 <kahwaji.anthony@gmail.com>
2019-10-15 17:09:15 +0200

    test set interval

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,29 @@
 `use strict`;
 
-const buttonBack = document.querySelector('#Back');
-const buttonNext = document.querySelector('#Next');
-let li = document.querySelectorAll('li');
+const buttonBack = document.querySelector("#Back");
+const buttonNext = document.querySelector("#Next");
+let li = document.querySelectorAll("li");
 let i = 0;
 
-
 function BackImageSlice() {
-    li[i].classList.remove('visible'); i--;
-    if (i < 0) {
-        i = li.length-1;
-       }
-     li[i].classList.add('visible');
+	li[i].classList.remove("visible");
+	i--;
+	if (i < 0) {
+		i = li.length - 1;
+	}
+	li[i].classList.add("visible");
 }
 
 function NextImageSlice() {
-    li[i].classList.remove('visible'); i++;
-    if (i >= li.length) {
-        i = 0;
-       }
-     li[i].classList.add('visible');
+	li[i].classList.remove("visible");
+	i++;
+	if (i >= li.length) {
+		i = 0;
+	}
+	li[i].classList.add("visible");
 }
 
-setInterval(NextImageSlice, 3000);
-
+setInterval(NextImageSlice, 100);
 
 buttonBack.addEventListener("click", BackImageSlice);
 buttonNext.addEventListener("click", NextImageSlice);
